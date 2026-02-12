16e76736c1c92e5a6a6702c81b7aa7840ef4022f
Rohith Sadanala <rohithsadanala123@gmail.com>
2026-02-12 00:27:34 -0600

    Streamlit updates

--- Services/config_loader.py
+++ Services/config_loader.py
@@ -1,11 +1,14 @@
-from dotenv import load_dotenv
 import os
+import streamlit as st
+from dotenv import load_dotenv
 
-load_dotenv()  # loads your .env file
+load_dotenv()
 
-def get_config(key: str):
-    """Get a configuration value from environment variables"""
-    value = os.getenv(key)
-    if value is None:
-        raise ValueError(f"Config key '{key}' not found in environment variables")
-    return value
+
+def get_config(key_name):
+    # 1. First, check if we are on Streamlit Cloud
+    if key_name in st.secrets:
+        return st.secrets[key_name]
+
+    # 2. Otherwise, check local .env
+    return os.getenv(key_name)
